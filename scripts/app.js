5023e5d3b7a67b0d6e2e3834c19e71a25d803b40
Alina_Em <alina@wavysys@com>
2021-08-04 15:03:03 +0300

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,59 @@
+//DOM queries
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMessage = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+
+//add a new chat
+newChatForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+    .then(() => {
+        newChatForm.reset(e.target.getAttribute('id'));
+    })
+    .catch(error => 
+        console.log(error));
+});
+// update username
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    newNameForm.reset();
+
+    //show and hifd update message
+    updateMessage.innerText = `Your name was updated to ${newName}`;
+
+    setTimeout(() => {
+        updateMessage.innerText = '';
+    }, 3000);
+});
+
+//update the chatroom
+
+rooms.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON') {
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute('id'));
+        chatroom.getChats(chat => chatUI.render(chat));
+
+    }
+});
+
+//check local storage for a name
+
+const username = localStorage.username ? localStorage.username : 'anon';
+
+//class instences
+
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('gaming', username);
+console.log(chatroom);
+
+//get the chat and render
+chatroom.getChats(data =>{
+    chatUI.render(data);
+});
+
